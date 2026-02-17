79d00d3e02337b16d978fcd53b33a01f5aa7d177
jjoseramoss <jr29beto12@gmail.com>
2026-02-16 21:25:28 -0600

    updated logTransaction function

--- frontend/app/lib/actions.ts
+++ frontend/app/lib/actions.ts
@@ -41,27 +41,48 @@ export async function logTransaction(formData: FormData){
     const supabase = await createClient()
 
     // 1. Get current user information
-    const { data: { user } } = await supabase.auth.getUser()
+    const { data: { user } } = await supabase.auth.getUser()    
 
+    
     if(!user) {
         throw new Error('You must be logged in to update your budget.')
     }
+
+    // 2. Get current budget
+    const {data: profile } = await supabase
+        .from("profiles")
+        .select("monthly_budget, pet_type")
+        .eq("id", user.id) 
+        .single()
 
     const amount = formData.get('amount') as string
     const category = formData.get('category') as string
     const description = formData.get('description') as string
 
-    if(amount){
-        const amountVal = parseFloat(amount)
-        //2. Update ONLY the row where ID matches logged in user
-        const { error } = await supabase
+    const amountVal = parseFloat(amount)
+    if(amountVal > 0){
+        //3. Add transaction ONLY the row where ID matches logged in user
+        const { error: insertError } = await supabase
             .from('transactions')
             .insert([
-                {user_id: user.id, amount: amount, category: category, description: description}
+                {user_id: user.id, amount: amountVal, category: category, description: description}
             ])
 
-        if (error){
-            console.error('Update failed: ', error.message)
+        if (insertError){
+            console.error('Update failed: ', insertError.message)
+            return
+        }
+
+        //4. Subtract from budget 
+        const newBudget = profile?.monthly_budget - amountVal;
+
+        const { error: updateError } = await supabase
+            .from('profiles')
+            .update({monthly_budget: newBudget})
+            .eq("id", user.id)
+
+        if (updateError){
+            console.error('Update failed: ', updateError.message)
             return
         }
 
